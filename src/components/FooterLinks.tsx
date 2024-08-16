9727c88bcc06eda0d9f84464eeb63f83e86fddd6
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2024-08-16 17:07:25 -0400

    feat: make footer link buttons responsive and always visible on mobile viewports

---
+++ src/components/FooterLinks.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '@/sass/style.scss'
+
+const FooterLinks = () => (
+  <div className="footer-links">
+    <a href="/resume">Resume</a>
+    <a href="https://linkedin.com">LinkedIn</a>
+    <a href="https://github.com">GitHub</a>
+    <a href="https://stackoverflow.com">StackOverflow</a>
+  </div>
+)
+
+export default FooterLinks
