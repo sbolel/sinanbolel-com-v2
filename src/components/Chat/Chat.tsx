734171f07e1bef75bad5e99ce5a5411b673c122d
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2024-08-16 16:10:25 -0400

    feat: replace first letter avatar with PersonIcon

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -14,6 +14,7 @@ import {
   Typography,
 } from '@mui/material'
 import Avatar from '@/components/mui/Avatar'
+import PersonIcon from '@mui/icons-material/Person'
 
 interface Message {
   body: string
@@ -70,7 +71,9 @@ const Chat: React.FC = () => {
           {messages.map((msg, index) => (
             <ListItem key={index} alignItems="flex-start">
               <ListItemAvatar>
-                <Avatar>{msg.body[0].toUpperCase()}</Avatar>
+                <Avatar>
+                  <PersonIcon />
+                </Avatar>
               </ListItemAvatar>
               <ListItemText
                 primary={msg.body}
