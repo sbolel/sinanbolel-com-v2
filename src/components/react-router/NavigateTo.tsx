3f8f7edd95dcda7a459cde66003655a95ef436ba
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2024-08-16 15:31:06 -0400

    chore: add assets

---
+++ src/components/react-router/NavigateTo.tsx
@@ -0,0 +1,20 @@
+/**
+ * Component that renders the react-router Navigate component to
+ * the input route.
+ * @module components/react-router/NavigateTo
+ *
+ * @see {@link dashboard/Routes} for usage.
+ * @see {@link https://reactrouter.com/web/api/Navigate} for documentation.
+ */
+import { Navigate } from 'react-router-dom'
+import { Routes } from '@/router/constants'
+
+/**
+ * Component that renders the Navigate component to redirect to the input route.
+ * @returns {JSX.Element}
+ */
+const NavigateTo: React.FC<{ route: Routes }> = ({ route }): JSX.Element => (
+  <Navigate to={route} replace={true} />
+)
+
+export default NavigateTo
